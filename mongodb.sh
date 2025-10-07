bfaed19df6d106ab59a3c3c25205e6b9d3d7a8a5
simhadri <vakacharlasimhadri@gmail.com>
2025-10-07 16:53:07 +0530

    sesssion-06

---
+++ mongodb.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then 
+        echo  -e "$2 ... $R is failure $N" | tee -a $LOG_FILE
+        exit 1
+    else 
+        echo  -e "$2 ... $G is SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repo.d/mongo.repo
+VALIDATE $? "adding mongo repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "Start MongoDB"
