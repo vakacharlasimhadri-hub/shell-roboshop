1f5ec67ecbb23113bdc1bfe7b95fa12bd6fed6fa
simhadri <vakacharlasimhadri@gmail.com>
2025-10-07 17:23:12 +0530

    sesssion-06

--- mongodb.sh
+++ mongodb.sh
@@ -27,7 +27,7 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     fi
 }
 
-cp mongo.repo /etc/yum.repo.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "adding mongo repo"
 
 dnf install mongodb-org -y &>>$LOG_FILE
