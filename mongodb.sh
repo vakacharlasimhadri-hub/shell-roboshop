db7ffa4c36842f529263ceaa4a5204834127e333
simhadri <vakacharlasimhadri@gmail.com>
2025-10-08 17:39:18 +0530

    sesssion-06

--- mongodb.sh
+++ mongodb.sh
@@ -36,5 +36,11 @@ VALIDATE $? "Installing MongoDB"
 systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enable MongoDB"
 
-systemctl start mongod &>>$LOG_FILE
+systemctl start mongod
 VALIDATE $? "Start MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to mongoDB"
+
+systemctl restart mongod
+VALIDATE $? "Restartedd MongoDB"
