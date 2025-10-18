176771562b480c6082a33abc54bc13d87211ed0d
simhadri <vakacharlasimhadri@gmail.com>
2025-10-18 21:12:15 +0530

    sesssion-06

--- redis.sh
+++ redis.sh
@@ -43,5 +43,5 @@ systemctl start redis &>>$LOG_FILE
 VALIDATE $? "Starting Redis"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME))
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
