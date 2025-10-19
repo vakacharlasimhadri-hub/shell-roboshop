093a66ebd46520f548d08096504f07ca6d3483c2
simhadri <vakacharlasimhadri@gmail.com>
2025-10-19 19:25:15 +0530

    session-06

--- shipping.sh
+++ shipping.sh
@@ -64,5 +64,13 @@ systemctl start shipping
 
 dnf install mysql -y
 
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
+if [ $? -ne 0 ]; then
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
+else
+    echo -e "Shipping data is already loaded ... $Y SKIPPING $N
+fi
 
-
+systemctl restart shipping
