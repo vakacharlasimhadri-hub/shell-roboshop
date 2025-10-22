c449c0bed1e54ace2eef2cf957e01f2cda88f169
simhadri <vakacharlasimhadri@gmail.com>
2025-10-23 00:07:24 +0530

    session-06

--- shipping.sh
+++ shipping.sh
@@ -9,9 +9,9 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-MONGODB_HOST=172.31.23.161
+MONGODB_HOST=mongodb.dawsp86s.space
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-MYSQL_HOST=172.31.23.3
+MYSQL_HOST=mysql.dawsp86s.space
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
